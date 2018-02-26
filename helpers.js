d5154563e29af396379700b60d0ff8756db7ea2d
Steven <stevenjacobprice@gmail.com>
2018-02-25 18:04:09 -0600

    use let instead of var

--- helpers.js
+++ helpers.js
@@ -1,11 +1,10 @@
 /**
- * Simple function that checks element visibility
- * @param {Object} el element we want to check visibility of
- * @returns {boolean}
+ * Checks the visibility of a DOM element
+ * @param {Object} el dom element
  */
 function isVisible (el) {
-  var fromTop = el.getBoundingClientRect().top;
-  var eH = el.offsetHeight;
-  var wH = window.innerHeight;
+  let fromTop = el.getBoundingClientRect().top;
+  let eH = el.offsetHeight;
+  let wH = window.innerHeight;
   return ((wH - fromTop > 0 || fromTop < 0) && fromTop + eH > 0);
 }
